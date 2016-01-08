367d8a035a8aa27175841b237085dd09e8133439
rnkumar <raja.kumar@certesnetworks.com>
2016-01-08 17:15:18 +0530

    eee

--- app/controllers/books/index.js
+++ app/controllers/books/index.js
@@ -5,10 +5,7 @@ export default Ember.Controller.extend({
   new: Ember.computed.alias('controllers.books.new'),
   actions: {
     go2New: function (lbook) {
-      //Ember.inject.controller('books').set('ref', lbook);
       this.controllerFor('books/new').set('ref', lbook);
-      //Ember.Logger.info(this.controllerFor('books/new'));
-      //this.transitionTo("books.new");
       this.transitionToRoute('books.new');
     }
   }
